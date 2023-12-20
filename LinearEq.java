08764eb9e9f81d901cec2e123662efe806c85152
Tamir <tamir.gluzman@post.runi.ac.il>
2023-12-20 16:46:47 +0200

    Added missing println in LinearEq

--- LinearEq.java
+++ LinearEq.java
@@ -10,7 +10,8 @@ public class LinearEq {
 		double b = Integer.parseInt(args[1]);
 		double c = Integer.parseInt(args[2]);
 		double x = (c - b) / a;
-		
+
+		System.out.println(a + " * x + " + b + " = " + c);
         System.out.println("x = " + x);
     }
 }
